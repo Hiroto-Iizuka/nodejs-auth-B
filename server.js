a53a3d3163cf00a40f3cd7ac7551e1bb1dc81883
Hiroto-Iizuka <oroxith@gmail.com>
2022-10-05 21:44:05 +0900

    feat: バリデーション条件に引っかかるとコンソールにエラーメッセージを表示

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // load the things we need
 const express = require('express');
 const app = express();
-const { body, validationResult } = require('express-validator');
+const { check, validationResult } = require('express-validator');
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
@@ -21,26 +21,36 @@ app.get('/register', function(req, res) {
 app.use(express.urlencoded({ extended: true })) 
 app.post(
     '/register',
-    body('password').isLength({ min: 7 }),
-    (req, res) => {
-        console.log(req.body.username);
-        console.log(req.body.email);
-        console.log(req.body.password);    
+    [
+        check('username').not().isEmpty().withMessage('usernameが空です。'),
+        check('email').not().isEmpty().withMessage('emailが空です。'),
+        check('password').not().isEmpty().withMessage('passwordが空です。'),
+        check('password').isLength({ min: 7 }).withMessage('passwordは７文字以上必要です。'),
+        check('password').custom((value, { req }) => {
+            if (value === req.body.confirmPassword) {
+                return true;
+            }
+        }).withMessage('passwordとconfirmPasswordが一致しません。'),
+    ],
+    function (req, res) {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
+            let messages = [];
+            errors.errors.forEach((error) => {
+                messages.push(error.msg);
+            });
+            console.log(messages);
+            res.render('pages/register');
+        } else { 
+            res.render('pages/index');        
         }
+    }
     // 今回は登録不要なのでコンソールに表示
     // User.create({
     //     username: req.body.username,
     //     email: req.body.email,
     //     password: req.body.password,
     // }).then(res.render('pages/index'));
-    console.log(req.body.username);
-    console.log(req.body.email);
-    console.log(req.body.password);  
-    res.render('pages/index')
-    },
 );
 
 // home page
